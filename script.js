1743893dc38f6d40355e0db09966ecdb4932399d
ptlmeet27 <20124040@nuv.ac.in>
2022-06-25 03:02:26 +0530

    js

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-function dummy(){
-    document.getElementById('result-screen').innerHTML="jana soja lode.";
-    document.getElementById('input-screen').innerHTML = "🤣🖕";
+
+
+
+const lastdiplayvalue = document.getElementById("last-value");
+const currentdisplayvalue = document.getElementById("current-value");
+
+
+const allclear = document.getElementById("all-clear");
+const del = document.getElementById("delete");
+const equals = document.getElementById("equals");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operator")
+
+
+
+console.log(lastdiplayvalue,currentdisplayvalue);
+
+
+function add(x,y){
+    return x + y;
 }
+function subtract(x,y){
+    return x - y;
+}
+function multiply(x,y){
+    return x * y;
+}
+function divide(x,y){
+    return x / y;
+}
+
+function operate(operator,x,y){
+    if(operator == '+'){
+        add(x,y);
+    }
+    if(operator == '-'){
+        subtract(x,y);
+    }
+    if(operator == '*'){
+        multiply(x,y);
+    }
+    if(operator == '/'){
+        divide(x,y);
+    }
+    else{
+        console.log("invalid operator");
+        return -1;
+    }
+
+}
+
